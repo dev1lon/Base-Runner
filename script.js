80b5cf672f3091a1f29322ad2fc6273f095727ab
devilon <normis1337@gmail.com>
2026-04-28 01:40:11 +0300

    Update contract addresses after redeploy
    XPToken:          0x5B383568C06800D0C6C85Fd637306AFf5e143843
    CharacterUpgrade: 0xBfC4443E991D7aF434d930450Ea8aa778f2B5383
    GameCoin:         0xf111569425dA3CbCE407C16401aCb1663Dca054c (unchanged)

--- script.js
+++ script.js
@@ -231,7 +231,7 @@ const PAYMENTS_ABI = [
 
 // Character upgrade contracts (set after deploy)
 const GAMECOIN_ADDRESS          = "0xf111569425dA3CbCE407C16401aCb1663Dca054c";
-const CHARACTER_UPGRADE_ADDRESS = "0x5A307DF5720cFb965C4F25d6fBFBed74Af153Ee5";
+const CHARACTER_UPGRADE_ADDRESS = "0xBfC4443E991D7aF434d930450Ea8aa778f2B5383";
 
 const GAMECOIN_ABI = [
     "function balanceOf(address) view returns (uint256)",
